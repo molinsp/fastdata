ecee5d15f2a14d71180e097668dd29601c87d27f
Pere Molins Ayuso <molinsp@gmail.com>
2020-10-28 12:11:46 +0200

    adding extract number from string

--- fastdata/_nbdev.py
+++ fastdata/_nbdev.py
@@ -2,7 +2,8 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"FastDataUtilities": "00_core.ipynb"}
+index = {"FastDataUtilities": "00_core.ipynb",
+         "generate_function_call_from_form": "00_core.ipynb"}
 
 modules = ["core.py"]
 
